75f2670b3dcdf78043d8ac91e5d53c26230a63fe
Galina Kasatkina <Galina_Kasatkina@epam.com>
2020-08-15 03:17:45 +0200

    initial commit

---
+++ precache-manifest.575a6eddc1ae6e1f263850f9aa8d608a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d40affcec80794da9c7207fd3bd459e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e93bd6101c07be3bb60",
+    "url": "/static/css/main.ddc22c26.chunk.css"
+  },
+  {
+    "revision": "500714d1ca47f4f5db36",
+    "url": "/static/js/2.e38e69bf.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/static/js/2.e38e69bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e93bd6101c07be3bb60",
+    "url": "/static/js/main.a7066519.chunk.js"
+  },
+  {
+    "revision": "f45fdf72772949231e45",
+    "url": "/static/js/runtime-main.2da335c4.js"
+  },
+  {
+    "revision": "aabbc3a700782a03daa4c4f864afe37d",
+    "url": "/static/media/gradient.aabbc3a7.svg"
+  }
+]);
